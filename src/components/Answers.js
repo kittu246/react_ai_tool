3b449688b0be9b3095a7497eb829cc3316bd5a1e
kritika <kritikashyup18@gmail.com>
2026-05-13 13:09:34 +0530

    error corrected

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,9 +1,5 @@
 import { useEffect, useState } from "react";
 import { checkHeading, replaceHeading } from "../helper";
-import SyntaxHighlighter from "react-syntax-highlighter";
-
-import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
-import ReactMarkdown from "react-markdown";
 
 const Answers = ({ ans,type, index ,length}) => {
   const [heading, setHeading] = useState(false);
@@ -16,24 +12,7 @@ const Answers = ({ ans,type, index ,length}) => {
     }
   }, []);
 
-  const renderer ={
-    code({
-      node,inline,className,children,...props
-    }){
-      const match =/language-(\w+)/.exec(className || '');
-      return !inline &&match?(
-        <SyntaxHighlighter {...props} children={String(children).replace(/\n$/,'') }
-        language={match[1]}
-        style={dark}
-        PreTag="div"
-        />
 
-      
-      ):(
-        <code {...props} className={className}>{children}</code>
-      )
-    }
-  }
   return (
     <div>
       {index === 0 && length >1 ? (
@@ -41,7 +20,7 @@ const Answers = ({ ans,type, index ,length}) => {
       ) : heading || type=="q" ? (
         <span className="block py-2 text-sm dark:text-white text-zinc-600 ">{finalAns}</span>
       ) : (
-        <span className="text-xsm"><ReactMarkdown components={renderer}>{finalAns}</ReactMarkdown></span>
+        <span className="text-xsm">{finalAns}</span>
       )}
     </div>
   );
